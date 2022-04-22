3b259ce4feb7105fde6c88a4ffb83806c1973239
Ajay Mathew <ajayedathazhe@gmail.com>
2022-04-22 18:28:10 +0530

    my draft

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,33 @@
-const chatForm = document.getElementById('chat-form');
-const chatMessages = document.querySelector('.chat-messages');
-const roomName = document.getElementById('room-name');
-const userList = document.getElementById('users');
-
-// Get username and room from URL
-const { username, room } = Qs.parse(location.search, {
-  ignoreQueryPrefix: true,
-});
+const chatForm = document.getElementById("chat-form");
+const chatMessages = document.querySelector(".chat-messages");
+const roomName = document.getElementById("room-name");
+const userList = document.getElementById("users");
+const form = document.getElementById("form");
+const chat = document.querySelector(".chat-container");
+const join = document.querySelector(".join-container");
 
 const socket = io();
 
-// Join chatroom
-socket.emit('joinRoom', { username, room });
+// Get username and room from URL
+form.addEventListener("submit", (e) => {
+  join.classList.toggle("hidden");
+  chat.classList.toggle("hidden");
+  e.preventDefault();
+  const username = e.target.username.value;
+
+  // Join chatroom
+  socket.emit("joinRoom", { username });
+});
 
 // Get room and users
-socket.on('roomUsers', ({ room, users }) => {
+socket.on("roomUsers", ({ room, users }) => {
   outputRoomName(room);
   outputUsers(users);
 });
 
 // Message from server
-socket.on('message', (message) => {
-  console.log(message);
+socket.on("message", (message) => {
+  // console.log(message);
   outputMessage(message);
 
   // Scroll down
@@ -29,7 +35,7 @@ socket.on('message', (message) => {
 });
 
 // Message submit
-chatForm.addEventListener('submit', (e) => {
+chatForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   // Get message text
@@ -42,27 +48,27 @@ chatForm.addEventListener('submit', (e) => {
   }
 
   // Emit message to server
-  socket.emit('chatMessage', msg);
+  socket.emit("chatMessage", msg);
 
   // Clear input
-  e.target.elements.msg.value = '';
+  e.target.elements.msg.value = "";
   e.target.elements.msg.focus();
 });
 
 // Output message to DOM
 function outputMessage(message) {
-  const div = document.createElement('div');
-  div.classList.add('message');
-  const p = document.createElement('p');
-  p.classList.add('meta');
+  const div = document.createElement("div");
+  div.classList.add("message");
+  const p = document.createElement("p");
+  p.classList.add("meta");
   p.innerText = message.username;
-  p.innerHTML += `<span>${message.time}</span>`;
+  p.innerHTML += `<span>  ${message.time}</span>`;
   div.appendChild(p);
-  const para = document.createElement('p');
-  para.classList.add('text');
+  const para = document.createElement("p");
+  para.classList.add("text");
   para.innerText = message.text;
   div.appendChild(para);
-  document.querySelector('.chat-messages').appendChild(div);
+  document.querySelector(".chat-messages").appendChild(div);
 }
 
 // Add room name to DOM
@@ -72,19 +78,21 @@ function outputRoomName(room) {
 
 // Add users to DOM
 function outputUsers(users) {
-  userList.innerHTML = '';
+  userList.innerHTML = "";
   users.forEach((user) => {
-    const li = document.createElement('li');
+    const li = document.createElement("li");
     li.innerText = user.username;
     userList.appendChild(li);
   });
 }
 
 //Prompt the user before leave chat room
-document.getElementById('leave-btn').addEventListener('click', () => {
-  const leaveRoom = confirm('Are you sure you want to leave the chatroom?');
+document.getElementById("leave-btn").addEventListener("click", () => {
+  const leaveRoom = confirm("Are you sure you want to leave the chatroom?");
   if (leaveRoom) {
-    window.location = '../index.html';
+    window.location = "../";
   } else {
   }
 });
+
+// -------------------------------------------
--- utils/users.js
+++ utils/users.js
@@ -1,7 +1,21 @@
 const users = [];
+const rooms = [];
+const activeRooms = [];
 
 // Join user to chat
-function userJoin(id, username, room) {
+function userJoin(id, username) {
+  var room;
+  if (rooms.length === 0) {
+    room = randomRoom();
+    while (activeRooms.includes(room)) {
+      room = randomRoom();
+    }
+    rooms.push(room);
+  } else {
+    room = rooms.shift();
+    activeRooms.push(room);
+    // console.log(activeRooms);
+  }
   const user = { id, username, room };
 
   users.push(user);
@@ -11,26 +25,46 @@ function userJoin(id, username, room) {
 
 // Get current user
 function getCurrentUser(id) {
-  return users.find(user => user.id === id);
+  return users.find((user) => user.id === id);
 }
 
 // User leaves chat
 function userLeave(id) {
-  const index = users.findIndex(user => user.id === id);
+  const index = users.findIndex((user) => user.id === id);
 
   if (index !== -1) {
+    if (!rooms.includes(users[index].room)) {
+      rooms.push(users[index].room);
+    } else {
+      rooms.pop(users[index].room);
+      const j = rooms.indexOf(users[index].room);
+      if (j > -1) {
+        rooms.splice(j, 1);
+      }
+    }
+
+    const i = activeRooms.indexOf(users[index].room);
+    if (i > -1) {
+      activeRooms.splice(i, 1);
+    }
+    console.log(rooms);
     return users.splice(index, 1)[0];
   }
 }
 
 // Get room users
 function getRoomUsers(room) {
-  return users.filter(user => user.room === room);
+  return users.filter((user) => user.room === room);
 }
 
 module.exports = {
   userJoin,
   getCurrentUser,
   userLeave,
-  getRoomUsers
+  getRoomUsers,
 };
+
+// create random room number
+function randomRoom() {
+  return Math.floor(Math.random() * 1000);
+}
