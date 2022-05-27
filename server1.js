07f6fb03d0b8104ef7e3c415826e2b036d98629d
Ajay Mathew <ajayedathazhe@gmail.com>
2022-05-27 12:12:33 +0530

    bug fixed in config file.

--- server1.js
+++ server1.js
@@ -87,7 +87,7 @@ io.on("connection", (socket) => {
     }
   });
 });
-
-const PORT = process.env.PORT || 3000;
+console.log(process.env) 
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
